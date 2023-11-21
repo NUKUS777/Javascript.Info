3ee22ad96cafa5d3dc05673444de21e9bbcce0ef
NUKUS777 <dawletkenesbaev620@gmail.com>
2023-11-21 06:43:14 +0500

    Logical operators

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-//logical operators
+
+
+//Task
+
+const user = prompt('Who is there');
+
+if (user=='Admin') {
+    const password = prompt('Password')
+    if (password=='TheMaster'){
+        alert('Welcome')
+    }
+    else if (password) {
+        alert('Wrong password ')
+    }
+    else (
+        alert('Canceled')
+    )
+}
+else if (user) {
+    alert('I dont know you ')
+}
+else (
+    alert('Canceled')
+)
